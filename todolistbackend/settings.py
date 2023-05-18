619001ec1d4df04a1cc4ceb80424b1ffdc4c6c52
Alexandre Vermeersch <112808930+AlexVerm12@users.noreply.github.com>
2023-05-18 15:17:19 +0200

    added authentication, TodoItemview, serializers and more

--- todolistbackend/settings.py
+++ todolistbackend/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'django-insecure-0d)=g#)a_0srvn_&_qfc46wy5ed#v5=5rnu8%g$duck1!33-g9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost'
+]
+CORS_ALLOWED_ORIGINS = [    'http://localhost:4200',]
 
 
 # Application definition
@@ -39,9 +42,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'todolist',
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,3 +128,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
